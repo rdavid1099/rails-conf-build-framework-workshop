2f9793bc6ce65f84009768071468f7876852890a
Ryan Workman <rdavid1099@gmail.com>
2019-04-30 13:09:51 -0700

    Add meta programming to call and use controllers

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -6,7 +6,18 @@ module R00lz
     def call(env)
       query = get_query(env['QUERY_STRING'])
       output = query['output'] == 'true' ? env.keys.map { |key| "#{key} => #{env[key]}"}.unshift('<p>').push('</p>').join("<br>") : ''
-      [ 200, { 'Content-Type' => 'text/html' }, [query['body'].to_s, output] ]
+      klass, act = cont_and_act(env)
+      navbar = '<a href="/">HOME</a> | <a href="/q/a_quote">A Quote</a> | <a href="/q/another_quote">Another Quote</a>'
+      text = klass ? klass.new(env).send(act) : ''
+      [ 200, { 'Content-Type' => 'text/html' }, [navbar, text, query['body'].to_s, output] ]
+    end
+
+    def cont_and_act(env)
+      _, con, act, after = env["PATH_INFO"].split('/')
+      return [Object.const_get('RootController'), 'index'] if con.nil?
+
+      con = con.capitalize + "Controller"
+      [ Object.const_get(con), act ]
     end
 
     def get_query(raw_string)
@@ -15,6 +26,14 @@ module R00lz
         result[split_query.first] = URI.decode(split_query.last)
         result
       end
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
