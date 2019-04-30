4603d32eb402368e88b09395206a861d1ce8fc06
Ryan Workman <rdavid1099@gmail.com>
2019-04-30 12:38:55 -0700

    Add basic env decoding and parsing to make display more customizable

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -1,6 +1,20 @@
+require 'uri'
 require "r00lz/version"
 
 module R00lz
-  class Error < StandardError; end
-  # Your code goes here...
+  class App
+    def call(env)
+      query = get_query(env['QUERY_STRING'])
+      output = query['output'] == 'true' ? env.keys.map { |key| "#{key} => #{env[key]}"}.unshift('<p>').push('</p>').join("<br>") : ''
+      [ 200, { 'Content-Type' => 'text/html' }, [query['body'].to_s, output] ]
+    end
+
+    def get_query(raw_string)
+      raw_string.split('&').reduce({}) do |result, query|
+        split_query = query.split('=')
+        result[split_query.first] = URI.decode(split_query.last)
+        result
+      end
+    end
+  end
 end
